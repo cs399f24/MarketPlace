0334b4cc83193d48683a0c55e60e776a2ca02745
Saul Sanchez <sanchezs@moravian.edu>
2024-10-30 00:01:26 -0400

    attempt to fix html 4

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 from flask import Flask, request, jsonify
 import dotenv
 from DynamoDB_MarketPlace import DynamoDBMarketPlace  # Assuming you have a module named market_place_db
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    user_id = request.json.get('user_id')
+    user_name = request.json.get('user_name')
+    email = request.json.get('email')
+    response = DynamoDBMarketPlace.create_user(user_id, user_name, email)
+    return jsonify(response)
+
+@app.route('/create_product', methods=['POST'])
+def create_product():
+    product_id = request.json.get('product_id')
+    product_name = request.json.get('product_name')
+    product_price = request.json.get('product_price')
+    product_owner = request.json.get('product_owner')
+    response = DynamoDBMarketPlace.create_product(product_id, product_name, product_price, product_owner)
+    return jsonify(response)
 
 @app.route('/get_user', methods=['GET'])
 def get_user():
