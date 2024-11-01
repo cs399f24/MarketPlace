534cee6d3768252c8d8491eb3e469c664346c1b1
Saul Sanchez <sanchezs@moravian.edu>
2024-11-01 11:07:07 -0400

    updated userdata file(working)

--- userdata.sh
+++ userdata.sh
@@ -2,7 +2,7 @@
 yum update -y
 yum install -y git python3
 git clone https://github.com/cs399f24/MarketPlace.git
-cd MarketPlace || exit 1  # Exit if cd fails
+cd MarketPlace || exit 1
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
