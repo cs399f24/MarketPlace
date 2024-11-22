a1312ac01dc700490c4dfee13e988b5fdc32f304
Alan <fuentesa02@moravian.edu>
2024-11-21 23:01:36 -0500

    Modified deploy script so that the man-pages do not pop up, forcing the user to manually exit out of each one. A final statement is echoed at the end signaling successful execution of the create scripts

--- deploy_scripts.sh
+++ deploy_scripts.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Disable AWS CLI pager
+export AWS_PAGER=""
+
+# Enable permissions
 chmod +x create_dynamodb_table.sh
 chmod +x create_get_all_product_lambda.sh
 chmod +x create_get_product_lambda.sh
@@ -8,6 +12,7 @@ chmod +x create_post_product_lambda.sh
 chmod +x create_post_user_lambda.sh
 # add api permissions when working
 
+# Launch create scripts
 ./create_dynamodb_table.sh  
 ./create_get_all_product_lambda.sh
 ./create_get_product_lambda.sh
@@ -16,4 +21,4 @@ chmod +x create_post_user_lambda.sh
 ./create_post_user_lambda.sh
 # add api deployment when working
 
-     
+echo "DynamoDB Table and Lambda Functions successfully created!"   
